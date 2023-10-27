99c77fd4db44dd414029b1b75b26796fbe62455c
Coloradoand <coloradoand@gmail.com>
2023-10-26 21:49:09 -0500

    Contenido en index Informacion en local storage

---
+++ js/signup.js
@@ -0,0 +1,32 @@
+class Usuario {
+    constructor(nombre, apellido, correo, password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.password = password;
+    }
+}
+
+let listUsuarios = [];
+if (JSON.parse(localStorage.getItem("listUsuarios"))) {
+    listUsuarios = JSON.parse(localStorage.getItem("listUsuarios"));
+}
+
+
+const formRegistro = document.getElementById("registro");
+
+formRegistro.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const nombreObtenido = document.getElementById("name").value;
+    const apellidoObtenido = document.getElementById("apellidos").value;
+    const correoObtenido = document.getElementById("email").value;
+    const passwordObtenida = document.getElementById("pass").value;
+
+    let newUsuario = new Usuario(nombreObtenido, apellidoObtenido, correoObtenido, passwordObtenida);
+
+    listUsuarios.push(newUsuario);
+
+    localStorage.setItem("listUsuarios", JSON.stringify(listUsuarios));
+
+    window.location.href = "/login.html";
+});
